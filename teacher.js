27495a6abe8f2ecaca97386f6ba561ee63904f77
vivekanandashiksha0508 <vivekanandashiksha0508@gmail.com>
2026-07-03 14:12:32 +0530

    Update teacher.js

--- teacher.js
+++ teacher.js
@@ -1,1 +1,24 @@
+function loginTeacher(){
 
+let pass=document.getElementById("password").value;
+
+if(pass=="dhruv058605remo"){
+
+window.location="teacher-dashboard.html";
+
+}
+
+else{
+
+document.getElementById("msg").innerHTML=
+"😄 Abhi baccha hai...<br>Ja re Student Portal me.";
+
+setTimeout(function(){
+
+window.location="student.html";
+
+},2500);
+
+}
+
+}
